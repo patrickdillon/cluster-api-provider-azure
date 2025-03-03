fca4af87658869486f086c1eeda537e1239a3992
Patrick Dillon <padillon@redhat.com>
2025-03-03 14:55:23 -0500

    UPSTREAM: 5532: Skip privatedns zones on Azure Stack
    Azure Stack Hub does not support private dns zones, so skip them.

--- azure/scope/cluster.go
+++ azure/scope/cluster.go
@@ -558,7 +558,7 @@ func (s *ClusterScope) VNetSpec() azure.ASOResourceSpecGetter[*asonetworkv1api20
 
 // PrivateDNSSpec returns the private dns zone spec.
 func (s *ClusterScope) PrivateDNSSpec() (zoneSpec azure.ResourceSpecGetter, linkSpec, recordSpec []azure.ResourceSpecGetter) {
-	if s.IsAPIServerPrivate() {
+	if s.IsAPIServerPrivate() && !s.IsHybridEnvironment() {
 		zone := privatedns.ZoneSpec{
 			Name:           s.GetPrivateDNSZoneName(),
 			ResourceGroup:  s.ResourceGroup(),
@@ -1225,4 +1225,9 @@ func (s *ClusterScope) getLastAppliedSecurityRules(nsgName string) map[string]in
 		lastAppliedSecurityRules = map[string]interface{}{}
 	}
 	return lastAppliedSecurityRules
+}
+
+// IsHybridEnvironment returns true if the cluster is running on Azure Stack.
+func (s *ClusterScope) IsHybridEnvironment() bool {
+	return strings.EqualFold(s.Environment.Name, azure.StackCloudName)
 }
